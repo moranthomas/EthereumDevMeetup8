f75f7cac63021699e7a8389afbe86ce6a14fab87
moranthomas <thomas@optimizme.com>
2021-03-28 18:12:50 +0100

    adding basic Transfer Funds

--- ui/src/Form.jsx
+++ ui/src/Form.jsx
@@ -45,6 +45,50 @@ export class Form extends Component {
 
     }
 
+    transferFunds(event)    {
+
+        console.log('transferring!');
+
+        /* Sending ether directly from one address to another
+        var txnObject = {
+            "from":"0x9b7421fC327E1B5123Ff9aDDD4B21d44557a3a13",
+            "to":"0x36c67ACAB3FA24b3Fd100437786F1314FD860921",
+            "value": web3.utils.toWei(1, 'ether'),
+            "gas":4712388,
+            gasPrice: 100000000000
+        };
+        web3.eth.sendTransaction(txnObject, function(error, result){console.log('transaction sent : ' + result);});
+        */
+
+        const _from = "0x7b1982A914452a38163714ab9C3671928C863a1D";
+        const _to = "0x8Ad3Ea4FE47557784BD1003864876FbC7d3ec879";
+        const _amount = "10";
+        let self = this;
+        var txnObject = {
+            "from":_from,
+            "to": _to,
+            "value": Web3.utils.toWei(_amount,'ether'),
+            "gas": 21000,         //(optional)
+            // "gasPrice": 4500000,  (optional)
+            // "data": 'For testing' (optional)
+            // "nonce": 10           (optional)
+        };
+
+        Web3.eth.sendTransaction(txnObject, function(error, result){
+            if(error){
+                console.log( "Transaction error" ,error);
+                self.$refs.resultRef.value = "Transaction Failed";
+            }
+            else{
+                //Get transaction hash
+                self.$refs.txHashRef.value = result;
+                self.$refs.resultRef.value = "Transaction Succeeded!";
+            }
+        });
+    }
+
+
+
     async loadBlockchainData() {
         console.log(this.addressIndex);
         let web3Provider = new Web3.providers.HttpProvider(this.ganacheUrl);
@@ -87,17 +131,6 @@ export class Form extends Component {
         const inputStyle = { padding: '5px', margin: '30px' };
         const accountsStyle = { fontSize: 16 };
 
-        /* Sending ether from one address to another
-        var txnObject = {
-            "from":"0x9b7421fC327E1B5123Ff9aDDD4B21d44557a3a13",
-            "to":"0x36c67ACAB3FA24b3Fd100437786F1314FD860921",
-            "value": web3.utils.toWei(1, 'ether'),
-            "gas":4712388,
-            gasPrice: 100000000000
-        };
-        web3.eth.sendTransaction(txnObject, function(error, result){console.log('transaction sent : ' + result);});
-        */
-
         return (
             <div>
             <form onSubmit={this.handleSubmit}>
@@ -108,6 +141,53 @@ export class Form extends Component {
                 <p style = {accountsStyle} >Your account: {this.state.account.substring(0,13)}</p>
                 <p style = {accountsStyle} >Your account balance: {this.state.accountBalance} Eth </p>
             </form>
+
+            <div>
+            <div class="row">
+                <div class="col-md-6">
+                    <label for="name" class="col-lg-2 control-label"><h4>Transfer</h4></label>
+                </div>
+                <div class="col-md-6">
+                   <p>&nbsp;</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                     <p>From: <input ref="fromRef" id="From" type="text" /> </p>
+                </div>
+            </div>
+            <div class="row">
+                 <div class="col-md-12">
+                    <p>To:<input id="To" ref="toRef" type="text" /> </p>
+                </div>
+            </div>
+              <div class="row">
+                 <div class="col-md-6">
+                   <p>Amount:<input id="Amount" ref="amountRef" type="text"/></p>
+                </div>
+                <div class="col-md-6">
+                    <button id="Transfer" onClick={this.transferFunds}>Transfer</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <p>Transaction Hash : </p>
+                </div>
+                <div class="col-md-8">
+                    <input id="Tx" ref="txHashRef" type="text" />
+                </div>
+            </div>
+                        <div class="row">
+                <div class="col-md-4">
+                    <p>Result : </p>
+                </div>
+                <div class="col-md-8">
+                    <input disabled ref="resultRef" id="Result" type="text" />
+                </div>
+            </div>
+
+        </div>
+
             </div>
         )
     }
