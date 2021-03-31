c592245fda2a54e189853b2a3c726ff3b6387b5b
moranthomas <thomas@optimizme.com>
2021-03-31 09:45:45 +0100

    form handling

--- ui/src/Form.jsx
+++ ui/src/Form.jsx
@@ -7,6 +7,7 @@ import { contractAbi, contractAddress } from './config';
 
 export class Form extends Component {
 
+
     componentWillMount() {
         this.addressIndex = 1;
         this.ganacheUrl = "http://localhost:7545";
@@ -14,15 +15,15 @@ export class Form extends Component {
         //this.getContractABI('../../Wallet/build/contracts/Wallet.json');
         this.getContractAbiFromConfig();
         this.getWalletAddressFromConfig();
-
+        this.createContract();
     }
 
     constructor(props) {
         super(props);
         this.state = {value: ''};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeDepositDAI = this.handleChangeDepositDAI.bind(this);
+        this.handleSubmitDepositDAI = this.handleSubmitDepositDAI.bind(this);
 
         this.state = {
             account: '',
@@ -30,22 +31,12 @@ export class Form extends Component {
             contractAddress: '',
             contractInstance: '',
             accountBalance: '',
+            amount: '',
         };
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        this.createContract();
-        let result = this.state.contractInstance.payMe.sendTransaction(this.state.account, this.state.value);
-        alert('This amount of DAI was deposited : ' + this.state.value);
-
-    }
-
-    transferFunds(event)    {
+    async transferFunds(event)    {
 
         event.preventDefault();
         console.log('transferring!');
@@ -53,17 +44,8 @@ export class Form extends Component {
         let web3Provider = new Web3.providers.HttpProvider(this.ganacheUrl);
         const web3 = new Web3(web3Provider);
 
-
-        /* Sending ether directly from one address to another
-        var txnObject = {
-            "from":"0x9b7421fC327E1B5123Ff9aDDD4B21d44557a3a13",
-            "to":"0x36c67ACAB3FA24b3Fd100437786F1314FD860921",
-            "value": web3.utils.toWei(1, 'ether'),
-            "gas":4712388,
-            gasPrice: 100000000000
-        };
-        web3.eth.sendTransaction(txnObject, function(error, result){console.log('transaction sent : ' + result);});
-        */
+        console.log(web3);
+        console.log(web3.eth);
 
         const _from = "0x7b1982A914452a38163714ab9C3671928C863a1D";
         const _to = "0x8Ad3Ea4FE47557784BD1003864876FbC7d3ec879";
@@ -79,30 +61,30 @@ export class Form extends Component {
             // "nonce": 10           (optional)
         };
 
-        web3.eth.sendTransaction(txnObject, function(error, result){
-            if(error){
-                console.log( "Transaction error" ,error);
-                this.resultRef.value = "Transaction Failed";
-            }
-            else{
-                //Get transaction hash
-                this.txHashRef.value = result;
-                this.resultRef.value = "Transaction Succeeded!";
-            }
-        });
+        web3.eth.sendTransaction(txnObject, function(error, result) {console.log('done'); });
+
+        // web3.eth.sendTransaction(txnObject, function(error, result){
+        //     if(error){
+        //         console.log( "Transaction error" ,error);
+        //         this.resultRef.value = "Transaction Failed";
+        //     }
+        //     else{
+        //         //Get transaction hash
+        //         this.txHashRef.value = result;
+        //         this.resultRef.value = "Transaction Succeeded!";
+        //     }
+        // });
     }
 
 
 
     async loadBlockchainData() {
-        console.log(this.addressIndex);
         let web3Provider = new Web3.providers.HttpProvider(this.ganacheUrl);
         const web3 = new Web3(web3Provider);
         const accounts = await web3.eth.getAccounts();
         const balance = await web3.eth.getBalance(accounts[this.addressIndex]);
         const balanceInEth = web3.utils.fromWei(balance, 'ether');
-        console.log(accounts);
-        console.log(balance);
+
         this.setState({ account: accounts[this.addressIndex], accountBalance: balanceInEth});
     }
 
@@ -132,15 +114,36 @@ export class Form extends Component {
     }
 
 
+    handleChangeAmount(event) {
+        this.setState({ amount: event.target.value });
+    }
+
+    handleSubmitAmount(event) {
+        alert(`Amt submiited: ${this.state.amount}`);
+        event.preventDefault();
+    }
+
+    handleChangeDepositDAI(event) {
+        this.setState({value: event.target.value});
+    }
+
+    async handleSubmitDepositDAI(event) {
+        event.preventDefault();
+        // console.log(this.web3);
+        console.log(this.state.contractInstance.methods);
+        let result = this.state.contractInstance.methods.payMe.sendTransaction(this.state.account, this.state.value);
+        alert('This amount of DAI was deposited : ' + this.state.value);
+    }
+
     render() {
-        const inputStyle = { padding: '5px', margin: '30px' };
-        const accountsStyle = { fontSize: 16 };
+        const inputStyle = { padding: '5px', marginLeft: '30px', marginRight: '30px' };
+        const accountsStyle = { fontSize: 16, marginBottom: '15px' };
 
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmitDepositDAI}>
                     <label > Deposit DAI:
-                        <input style={inputStyle} type="text" value={this.state.value} onChange={this.handleChange} />
+                        <input style={inputStyle} type="text" value={this.state.value} onChange={this.handleChangeDepositDAI} />
                     </label>
                     <input type="submit" value="Submit" />
                     <p style = {accountsStyle} >Your account: {this.state.account.substring(0,13)}</p>
@@ -148,41 +151,61 @@ export class Form extends Component {
                 </form>
 
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="name" class="col-lg-2 control-label"><h4>Transfer</h4></label>
+                {/* <form onSubmit={this.submitAmount}>
+                    <div className="form-control">
+                        <label htmlFor="text">Add Amount </label>
+                        <input style={inputStyle} type="text" value={this.amount}
+                            onChange={(e) => this.submitAmount(e.target.value)}
+                            placeholder="Enter Amount ...">
+                        </input>
+                        <input type="submit" value="Submit" />
                     </div>
-                </div>
-                <div style = {accountsStyle} class="row">
-                    <div class="col-md-12">
-                        <p>From: <input ref="fromRef" id="From" type="text" /> </p>
+                </form> */}
+
+                <form onSubmit={this.handleSubmitAmount.bind(this)}>
+                    <div className="form-control">
+                        <label htmlFor="text">Add Amount </label>
+                        <input style={inputStyle} type="text" value={this.state.amount}
+                        onChange={this.handleChangeAmount.bind(this)}
+                        placeholder="Enter Amount ...">
+                        </input>
+                        <input type="submit" value="Submit" />
                     </div>
-                </div>
-                <div style = {accountsStyle} class="row">
-                    <div class="col-md-12">
-                        <p>To:<input id="To" ref="toRef" type="text" /> </p>
+                </form>
+
+                <form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="name" class="col-lg-2 control-label"><h4>Transfer</h4></label>
+                        </div>
                     </div>
-                </div>
-                <div style = {accountsStyle} class="row">
-                    <div class="col-md-6">
-                    <p>Amount:<input id="Amount" ref="amountRef" type="text"/></p>
+                    <div style = {accountsStyle} class="row">
+                        <div class="col-md-12">
+                            From: <input style={inputStyle} ref="fromRef" id="From" type="text" />
+                            To:<input style={inputStyle} id="To" ref="toRef" type="text" />
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <button id="Transfer" onClick={this.transferFunds}>Transfer</button>
+
+                    <div style = {accountsStyle} class="row">
+                        <div class="col-md-6">
+                            Amount:<input style={inputStyle} id="Amount" ref="amountRef" type="text"/>
+                            <button id="Transfer" onClick={this.transferFunds}>Transfer</button>
+                        </div>
                     </div>
-                </div>
-                <div style = {accountsStyle} class="row">
-                    <div class="col-md-4">
-                        <p>Transaction Hash :  <input id="Tx" ref="txHashRef" type="text" />
-                        </p>
+                    <div style = {accountsStyle} class="row">
+                        <div class="col-md-4">
+                            <p>Transaction Hash :  <input style={inputStyle} id="Tx" ref="txHashRef" type="text" />
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div style = {accountsStyle} class="row">
-                    <div class="col-md-4">
-                        <p>Result : <input disabled ref="resultRef" id="Result" type="text" />
-                        </p>
+                    <div style = {accountsStyle} class="row">
+                        <div class="col-md-4">
+                            <p>Result : <input style={inputStyle} disabled ref="resultRef" id="Result" type="text" />
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </form>
+
             </div>
         )
     }
